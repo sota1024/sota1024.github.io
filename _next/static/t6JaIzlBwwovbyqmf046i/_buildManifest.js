39bc9833afe4a849368403532c048756dbb10ee6
sota1024 <sota1024@users.noreply.github.com>
2022-03-06 12:33:29 +0000

    deploy: 34429b00d0fe77b13acdbded1a6bdd8b05b0c3d3

---
+++ _next/static/t6JaIzlBwwovbyqmf046i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5fe8e5da20be988c.js"],"/_error":["static/chunks/pages/_error-1f95674706ce531a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
